f120009d884bc021c6f016c6c4cc2a0ca09f9edf
aiberry <krkow@mail.ru>
2017-02-02 00:39:00 +0300

    head and tail withour error

--- src/linked-list.js
+++ src/linked-list.js
@@ -7,7 +7,7 @@ class LinkedList {
     if ( this.length==0)
       {this._head=new Node; this._head.data=data; this._head.next=null; this._head.prev=null}
       this._tail=this._head; }
-    else {var ind=this.length+1; this.ind= new Node; this._tail.data=data; this._tail.prev=
+    
 
 
 
