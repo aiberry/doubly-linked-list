1c9f87ac17c2e5a1c5c1e744486192d4ce9c1461
aiberry <krkow@mail.ru>
2017-02-01 12:45:13 +0300

    second point

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,10 +3,15 @@ const Node = require('./node');
 class LinkedList {
     constructor() {this.length=0}
 
-    append(data) {if (this.length==0) this._head=data;
-                  if (this.length==0) this._tail=data}
+    append(data) {
+      if ( this.length==0)
 
-    head() {}
+      {this._head=new Node(this.data=data, this.prev=null, this.next=null);
+      this._tail=new Node(this.data=data, this.prev=null, this.next=null)}}
+
+
+
+    head() {this.list=this.list+new Node(data=this.data prev=null next=null)}
 
     tail() {}
 
