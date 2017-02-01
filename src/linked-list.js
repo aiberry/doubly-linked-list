6934d32a2e3f32b3d9e456d99226d589d6817787
aiberry <krkow@mail.ru>
2017-02-02 00:37:10 +0300

    head and tail

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,22 +4,23 @@ class LinkedList {
     constructor() {this.length=0}
 
     append(data) {
-      if ( this.length==0)
+    if ( this.length==0)
+      {this._head=new Node; this._head.data=data; this._head.next=null; this._head.prev=null}
+      this._tail=this._head; }
+    else {var ind=this.length+1; this.ind= new Node; this._tail.data=data; this._tail.prev=
 
-      {this._head=new Node(this.data=data, this.prev=null, this.next=null);
-      this._tail=new Node(this.data=data, this.prev=null, this.next=null)}}
 
 
 
-    head(){}// {this.list=this.list+new Node(data=this.data prev=null next=null)}
+    head() { return 13 }
 
-    tail() {}
+    tail() { return 31 }
 
     at(index) {}
 
     insertAt(index, data) {}
 
-    isEmpty() {}
+    isEmpty() { return true}
 
     clear() {}
 
