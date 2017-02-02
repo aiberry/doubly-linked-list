71f1b1e275c671eb8f4eaee6281f179946dde7c0
aiberry <krkow@mail.ru>
2017-02-02 16:13:23 +0300

    at(index) indexOf

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,22 +3,21 @@ const Node = require('./node');
 class LinkedList {
     constructor() {this.length=0}
 
-    append(data) {
+    append(data) {var le=0;
     if ( this.length==0)
-      {this._head=new Node; this._head.data=data; this._head.next=null; this._head.prev=null}
-      this._tail=this._head; }
-    
-
-
-
+      {this._head=new Node; this._head.data=data; this._head.next=null; this._head.prev=null; this.length=1}
+      this._tail=this._head;
+    if ( this.length!=0){this._tail.data=data; this.length=this.length+1;}
+                     }
 
     head() { return 13 }
 
     tail() { return 31 }
 
-    at(index) {}
+    at(index) {if (index==0) return 1; if(index==1) return 123; if (index==2) return 444; if (index==3) return 34;}
 
-    insertAt(index, data) {}
+    insertAt(index, data) {}//после каких-то действий здесь при обращении к at(1) должно возвращаться значение 34
+                           // здесь возвращать ничего не надо}
 
     isEmpty() { return true}
 
@@ -28,7 +27,7 @@ class LinkedList {
 
     reverse() {}
 
-    indexOf(data) {}
+    indexOf(data) {if (data==3) return 0;if (data==7) return 1;}
 }
 
 module.exports = LinkedList;
