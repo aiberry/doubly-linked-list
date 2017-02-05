5cbee722059101d18c1dc67debb6ef8976e95db6
aiberry <krkow@mail.ru>
2017-02-05 19:10:17 +0300

    changed algorithms

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,33 +1,53 @@
 const Node = require('./node');
 
-class LinkedList {
-    constructor() {this.length=0}
+class LinkedList {                  //assign 0 to this.length
+    constructor() {
 
-    append(data) {var le=0;
-    if ( this.length==0)
-      {this._head=new Node; this._head.data=data; this._head.next=null; this._head.prev=null; this.length=1}
-      this._tail=this._head;
-    if ( this.length!=0){this._tail.data=data; this.length=this.length+1;}
+        this.length=0;
+
+    }
+
+    append(data) {                  //should assign any nodes to this._head and this._tail if list is empty
+                                    //should add new data to the end of list
+        if (this.length>1) {this[this.length-1]= this._tail; this._tail=new Node(data); this.length++};
+
+        if (this.length==1) {this._tail=new Node(data); this.length++};
+
+        if (this.length==0) {this._head=new Node(data); this._tail=this._head; this.length++};
+
                      }
 
-    head() { return 13 }
+    head() { return this._head.data  }         //should return data from the this.head
 
-    tail() { return 31 }
+    tail() { return this._tail.data }            //should return data from the this.tail
 
-    at(index) {if (index==0) return 1; if(index==1) return 123; if (index==2) return 444; if (index==3) return 34;}
+    at(index) {
+        if (index == 0) {
+            return this._head.data
+        }
+        else { if (index==this.length-1) {return this._tail.data}
+        else return this[index].data
+        }
 
-    insertAt(index, data) {}//после каких-то действий здесь при обращении к at(1) должно возвращаться значение 34
-                           // здесь возвращать ничего не надо}
+    }
 
-    isEmpty() { return true}
+                                     //should return Node.data by index
 
-    clear() {}
+    //{if (index==0)        return 1; if(index==1) return 123; if (index==2) return 444; if (index==3) return 34;}
 
-    deleteAt(index) {}
+    insertAt(index, data) {}        //should insert data by index
 
-    reverse() {}
 
-    indexOf(data) {if (data==3) return 0;if (data==7) return 1;}
+    isEmpty() { if (this.length==0) {return true} else {return false}}        //should return true if list is empty
+
+    clear() {this.length=0; this._head.data=null; this._tail.data=null;}                      //should clear the list
+
+    deleteAt(index) {}              //should delete element by index
+
+    reverse() {}                    //should reverse the list
+
+    indexOf(data)   {if (data==(this._head.data)) {return 0}}                //should return index of element if data is found
+   // {if (data==3) return 0;if (data==7) return 1;}
 }
 
 module.exports = LinkedList;
